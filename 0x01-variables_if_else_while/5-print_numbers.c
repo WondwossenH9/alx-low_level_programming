b52e1fab9b230d49c7de45508719c6039f71948d
Wondwossen Hailu <wondwossenhailu7@gmail.com>
2022-12-21 13:51:32 -0800

    fixed betty typo

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,5 +11,6 @@ int main(void)
 	for (a = 0; a < 10; a++)
 		printf("%d", a);
 	putchar('\n');
+
 	return (0);
 }
