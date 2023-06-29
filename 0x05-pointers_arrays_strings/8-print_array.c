05c698539ffc25e37022c419829f84471279b53f
Wondwossen <wondwossenhailu7@gmail.com>
2023-06-29 01:17:07 -0700

    fixed betty coding style

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_array - prints n elements of an array of integers, followed by a new line
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
  * @a: array name
  * @n: number of elements of the array
  * Return: a and n inputs
