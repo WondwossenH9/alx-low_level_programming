d5f5fafcc3adb6d6ae39b52594cee26ffa4e5c1c
Wondwossen <wondwossenhailu7@gmail.com>
2023-08-01 11:15:14 -0700

    fix logic error in if statement

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,13 @@ int print_last_digit(int n)
 	int i;
 
 	if (n < 0)
-	{
 		n = -n;
-		i = n % 10;
-	}
+	i = n % 10;
+
 	if (i < 0)
-	{
 		i = -i;
-		_putchar(i + '0');
-	}
+
+	_putchar(i + '0');
+
 	return (i);
 }
