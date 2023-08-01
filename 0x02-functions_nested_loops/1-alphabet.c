96b64f943f2d4db8413dba2d45a5b462aaf3e0af
Wondwossen <wondwossenhailu7@gmail.com>
2023-08-01 10:37:05 -0700

    Write a function that writes the alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,11 @@
 void print_alphabet(void)
 {
 	char alphabets;
-	
+
 	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 	{
 		_putchar(alphabets);
-		_putchar('\n');
 	}
+
+	_putchar('\n');
 }
