e3a39b237ed11e1351d3d7886e0a6a680522f5c4
Wondwossen <wondwossenhailu7@gmail.com>
2023-08-01 12:09:32 -0700

    fix code and betty error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,41 +6,33 @@
 void print_times_table(int n)
 {
 	int i, j, m;
-	
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
-                {
-			for (j = 0; j <= n;)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
+				_putchar(',');
+				_putchar(' ');
 				m = i * j;
-				if (j == 0)
-				{
-					_putchar(m + '0');
-				} else if (m < 10 && j != 0)
-				{
-					_putchar(',');
+				if (m <= 99)
 					_putchar(' ');
+				if (m <= 9)
 					_putchar(' ');
-					_putchar(m + '0');
-				} else if (m >= 10 && m < 100)
+				if (m >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((m / 10) + '0');
-					_putchar((m % 10) + '0');
-				} else if (m >= 100)
-				{
-						_putchar(',');
-						_putchar(' ');
-						_putchar((m / 100) + '0');
-						_putchar(((m / 10) % 10) + '0');
-						_putchar((m % 10) + '0');
+					_putchar((m / 100) + '0');
+					_putchar(((m / 10)) % 10 + '0');
 				}
+				else if (m <= 99 && m >= 10)
+				{
+					_putchar((m / 10) + '0');
+				}
+					_putchar((m % 10) + '0');
 			}
-			_putchar('\n');
+					_putchar('\n');
 		}
-
 	}
 }
