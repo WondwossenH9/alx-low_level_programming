15e1f9f768c17de8c11a4d03d2a2a32bbf6e6ac1
Wondwossen <wondwossenhailu7@gmail.com>
2023-08-02 10:22:51 -0700

    fix code

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,15 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int num;
 
-	for (c = 48; c <= 58; c++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (c != 50)
+		if (num != 2 && num != 4)
 		{
-			if (c != 52)
+			if (num != 2 && num != 4)
 			{
-				_putchar(c);
+				_putchar(num + '0');
 			}
 		}
 	}
