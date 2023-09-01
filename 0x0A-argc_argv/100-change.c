1e62004542da32c424e567ec5d836f06b0123fab
Wondwossen <wondwossenhailu7@gmail.com>
2023-09-01 04:35:10 -0700

    fix betty coding style

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,28 +19,28 @@ int main(int argc, char *argv[])
 	cents = atoi(argv[1]);
 	while (cents > 0)
 	{
-		 coins ++;
-		 if ((cents - 25) >= 0)
-		 {
-			 cents -= 25;
-			 continue;
-		 }
-		 if ((cents - 10) >= 0)
-		 {
-			 cents -= 10;
-			 continue;
-		 }
-		 if ((cents - 5) >= 0)
-		 {
-			 cents -= 5;
-			 continue;
-		 }
-		 if ((cents - 2) >= 0)
-		 {
-			 cents -= 2;
-			 continue;
-		 }
-		 cents--;
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
 	}
 	printf("%d\n", coins);
 	return (0);
