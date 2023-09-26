c57a137aef9d7a666837534808668bd519328679
Wondwossen <wondwossenhailu7@gmail.com>
2023-09-26 12:08:54 -0700

    fix code

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -5,16 +5,17 @@
  * @head: first node
  * Return: total of the sum of data in list
  */
+
 int sum_listint(listint_t *head)
 {
-	int total; = 0;
+	int sum = 0;
 	listint_t *temp = head;
 
 	while (temp)
 	{
-		total += temp->n;
+		sum += temp->n;
 		temp = temp->next;
 	}
 
-	return (total);
+	return (sum);
 }
