40202ac40fb1ecbd03f13e72427bf18f336e9672
Wondwossen <wondwossenhailu7@gmail.com>
2024-10-29 06:26:39 -0700

    Write a C program that prints exactly the given string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* standard out using puts */
+/**
+ * main - entry point for the program
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
