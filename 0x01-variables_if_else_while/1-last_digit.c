6cacd77d47499befa6e5d4fdef28a1c4706faf75
Wondwossen <wondwossenhailu7@gmail.com>
2024-10-29 12:04:50 -0700

    fix betty coding style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,29 +5,29 @@
 /**
  * main - entry point of the program
  * n: store the random number generated
- * last_digit: store last digit of n
+ * x: store last digit of n
  * Return: 0 on success
  */
 
 int main(void)
 {
-	int n, last_digit;
+	int n, x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	last_digit = n % 10;
-	if (last_digit > 5)
+	x = n % 10;
+	if (x > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
 	}
-	else if (last_digit == 0)
+	else if (x == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
-	else if (last_digit < 6 && last_digit != 0)
+	else if (x < 6 && x != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
 	return (0);
 }
