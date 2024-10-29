3ac94ca15fac9df857186c2eec842609289c0ffd
Wondwossen <wondwossenhailu7@gmail.com>
2024-10-29 06:31:33 -0700

    Write a C program that prints exactly the given strings using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* Standard out put using printf*/
+/**
+ * main - prints output using printf
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
+	return (0);
+}
